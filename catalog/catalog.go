7d2507d2c15cb1f9c77e2d8a0bfaee2ba4f142d9
Anna Hogger <ahogger@hm.edu>
2022-01-17 15:29:48 +0100

    Updated catalog

--- catalog/catalog.go
+++ catalog/catalog.go
@@ -4,19 +4,56 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
+	"github.com/go-redis/redis/v8"
 	"github.com/nats-io/nats.go"
 	"gitlab.lrz.de/vss/semester/ob-21ws/blatt-2/blatt2-gruppe14/api"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
 )
 
 type Server struct {
 	Nats      *nats.Conn
+	Redis     *redis.Client
 	Catalog   map[uint32]*api.NewCatalog
 	CatalogID uint32
 	api.UnimplementedCatalogServer
 }
 
-func (s Server) GetCatalogInfo(ctx context.Context, in *api.GetCatalog) (*api.CatalogReply, error) {
+func (s Server) getAvailability(ctx context.Context, in *api.GetCatalog) (answer bool) {
+	stock_redisVal := s.Redis.Get(context.TODO(), "stock")
+	if stock_redisVal == nil {
+		log.Fatal("service not registered")
+	}
+	stock_address, err := stock_redisVal.Result()
+	if err != nil {
+		log.Fatalf("error while trying to get the result %v", err)
+	}
+	stock_conn, err := grpc.Dial(stock_address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer stock_conn.Close()
+	stock := api.NewStockClient(stock_conn)
+	stock_ctx, stock_cancel := context.WithTimeout(context.Background(), time.Second)
+	defer stock_cancel()
+
+	stock_r, stock_err := stock.GetStock(stock_ctx, &api.ArticleID{Id: in.GetId()})
+	if stock_err != nil {
+		st, ok := status.FromError(stock_err)
+		if !ok {
+			log.Fatalf("An unexpected error occurred: %v", stock_err)
+		}
+		log.Printf("GetStock failed: %v", st.Message())
+	}
+	log.Printf("Article available in Stock: Id: %v, Available: %v", in.GetId(), stock_r.GetAnswer())
+
+	return stock_r.GetAnswer()
+
+}
+
+func (s Server) GetCatalogInfo(ctx context.Context, in *api.GetCatalog) (*api.CatalogReplyInfo, error) {
 	log.Printf("received request for the article with: id: %v", in.GetId())
 
 	err := s.Nats.Publish("log.catalog", []byte(fmt.Sprintf("received request for the article with: id: %v", in.GetId())))
@@ -39,7 +76,9 @@ func (s Server) GetCatalogInfo(ctx context.Context, in *api.GetCatalog) (*api.Ca
 		panic(err)
 	}
 
-	return &api.CatalogReply{Id: s.CatalogID, Name: out.GetName(), Description: out.GetDescription(), Price: out.GetPrice()}, nil
+	available := s.getAvailability(ctx, in)
+
+	return &api.CatalogReplyInfo{Id: s.CatalogID, Name: out.GetName(), Description: out.GetDescription(), Price: out.GetPrice(), Avalability: available}, nil
 }
 
 func (s Server) NewCatalogArticle(ctx context.Context, in *api.NewCatalog) (*api.CatalogReply, error) {
